e7cbf9dee0032f5e2fb446f95ac3c268a8ff954c
simransyan15 <79874338+simransyan15@users.noreply.github.com>
2021-12-09 14:54:06 -0800

    Add files via upload
    updated

--- simran/login_check.php
+++ simran/login_check.php
@@ -5,15 +5,22 @@ include("include/conn.php");
 if(isset($_POST['login'])){
 	$email=$_POST['email'];
 	$pwd=$_POST['pwd'];
-	$sql="SELECT * from admin where email='$email' and password='$pwd'";
+	$sql="SELECT * from users where email='$email' and password='$pwd' ";
 	$sql_find=mysqli_query($conn,$sql);
 	if(mysqli_num_rows($sql_find)>0){
-		$_SESSION['admin_login_email']=$email;
-		echo "<script>alert('Admin Loggedin')</script>";
-		echo "<script>window.location.href='home.php'</script>";
+		$fetch=mysqli_fetch_array($sql_find);
+		$status=$fetch['status'];
+		if($status=="Blocked"){
+			echo "<script>alert('User Blocked Please contact Administrator')</script>";
+			echo "<script>window.location.href='login.php'</script>";
+		}else{
+			$_SESSION['user_login_email']=$email;
+			echo "<script>alert('User Loggedin')</script>";
+			echo "<script>window.location.href='home.php'</script>";
+		}
 	}else{
-		echo "<script>alert('Incorrect Credentials Please re-login')</script>";
-		echo "<script>window.location.href='login.php'</script>";
+		echo "<script>alert('User Not Found!! Please Register')</script>";
+		echo "<script>window.location.href='register.php'</script>";
 
 	}
 }
